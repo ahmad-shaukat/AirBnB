5b09a9570b53b95804280a513e3177379d365fbf
ahmad-shaukat <ahmadshaukat@yahoo.com>
2023-03-18 14:16:14 -0400

    debugging

--- backend/db/migrations/20230318153014-create-spot-image.js
+++ backend/db/migrations/20230318153014-create-spot-image.js
@@ -26,7 +26,7 @@ module.exports = {
         type: Sequelize.STRING,
         allowNull: false
       },
-      prview: {
+      preview: {
         type: Sequelize.BOOLEAN,
         allowNull: false
       },
