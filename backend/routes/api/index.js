df9b99dd1cb3353af614569b17851421a8ca9d0c
ahmad-shaukat <ahmadshaukat@yahoo.com>
2023-03-23 18:10:36 -0400

    Add endpointes for Reviews

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -3,12 +3,16 @@ const spotRouter = require('./spot.js')
 const sessionRouter = require('./session.js');
 const usersRouter = require('./users.js')
 const { restoreUser } = require('../../utils/auth.js');
+const reviewRouter = require('./review')
 // Connect restoreUser middleware to the API router
   // If current user session is valid, set req.user to the user in the database
   // If current user session is not valid, set req.user to null
 
 router.use(restoreUser);
+
 router.use('/spots',spotRouter)
+
+router.use('/reviews', reviewRouter)
 
 router.use('/session', sessionRouter);
 
